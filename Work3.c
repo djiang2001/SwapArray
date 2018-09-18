d8fc56cb6bae7eee98a351a461a4df136fb19554
djiang2001 <djiang2@stuy.edu>
2018-09-18 08:22:51 -0400

    Did array swap

--- Work3.c
+++ Work3.c
@@ -3,12 +3,27 @@
 #include <time.h>
 
 int main(){
+  //First Array
   srand(time(NULL));
   int arr[10];
   int i;
   for(i = 0; i < 10;i++){
-    a[i] = rand();
-    
+    arr[i] = rand();
+  }
+  arr[9] = 0;
+
+  for(i = 0; i < 10; i++){
+    printf("Array %d %d \n", i, arr[i]);
   }
 
+  //Second Array
+  int arr2[10];
+  for(i = 9;i>=0; i--){
+    *(arr2 + i) = *(arr + (9-i));
+  }
+
+    for(i = 0; i < 10; i++){
+    printf("Array2 %d %d \n", i, arr2[i]);
+  }
 }
+
