accef2a3cd481102314a1666b6b44739288bebc0
djiang2 <djiang2>
2018-09-18 11:06:49 -0400

    Added new file

---
+++ Work3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+  srand(time(NULL));
+  int arr[10];
+  int i;
+  for(i = 0; i < 10;i++){
+    a[i] = rand();
+    
+  }
+
+}
